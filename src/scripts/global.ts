551b8fcfc1f688e0f183c7c1c2640d21a6b285c6
sutefu23 <sutefu23@gmail.com>
2023-06-28 13:26:29 +0900

    ナビゲーション調整ほか

--- src/scripts/global.ts
+++ src/scripts/global.ts
@@ -1,33 +1,35 @@
 // スムーススクロール
 const smoothScroll = document.querySelectorAll('a[href^="#"]');
-for (let i = 0; i < smoothScroll.length; i++){
-  smoothScroll[i].addEventListener('click', (e) => {
+for (let i = 0; i < smoothScroll.length; i++) {
+  smoothScroll[i].addEventListener("click", (e) => {
     e.preventDefault();
-    let href = smoothScroll[i].getAttribute('href');
-    if(!href) return;
-    let targetElement = document.getElementById(href.replace('#', ''));
+    let href = smoothScroll[i].getAttribute("href");
+    if (!href) return;
+    let targetElement = document.getElementById(href.replace("#", ""));
     const rect = targetElement?.getBoundingClientRect().top;
     const offset = window.pageYOffset;
     const gap = 60;
-    const target = (rect??0) + offset - gap;
+    const target = (rect ?? 0) + offset - gap;
     window.scrollTo({
       top: target,
-      behavior: 'smooth',
+      behavior: "smooth",
     });
   });
 }
 // スムースフェード
-const elements = document.querySelectorAll<HTMLElement>(".fade");
+const fadeElements = document.querySelectorAll<HTMLElement>(".fade");
 window.onscroll = () => {
-  elements.forEach((element) => {
-    const showPosition = element.dataset.showPosition ? Number(element.dataset.showPosition): 0;
+  fadeElements.forEach((element) => {
+    const showPosition = element.dataset.showPosition
+      ? Number(element.dataset.showPosition)
+      : 0;
     if (window.pageYOffset > showPosition) {
       element?.classList.add("show");
       element?.classList.remove("hide");
     } else {
       element?.classList.add("hide");
       element?.classList.remove("show");
-    }  
+    }
   });
 };
 
@@ -45,38 +47,40 @@ if (menuBtn) {
 
 function slideMenu(): void {
   if (body) {
-      body.classList.toggle('open');
+    body.classList.toggle("open");
   }
 
-  if (body && body.classList.contains('open')) {
-      if (menu) {
-          menu.style.transition = "right 0.1s ease-in-out";
-          menu.style.right = "0";
-          let overlay: HTMLElement = document.createElement('div');
-          overlay.id = 'overlay';
-          overlay.style.transition = "opacity 0.1s ease-in-out";
-          overlay.style.opacity = "0";
-          if (body) body.appendChild(overlay);
-          setTimeout(function() { overlay.style.opacity = "1"; }, 0); // Trigger fade in.
-          overlay.addEventListener('click', function() {
-              slideMenu();
-          });
-      }
+  if (body && body.classList.contains("open")) {
+    if (menu) {
+      menu.style.transition = "right 0.1s ease-in-out";
+      menu.style.right = "0";
+      let overlay: HTMLElement = document.createElement("div");
+      overlay.id = "overlay";
+      overlay.style.transition = "opacity 0.1s ease-in-out";
+      overlay.style.opacity = "0";
+      if (body) body.appendChild(overlay);
+      setTimeout(function () {
+        overlay.style.opacity = "1";
+      }, 0); // Trigger fade in.
+      overlay.addEventListener("click", function () {
+        slideMenu();
+      });
+    }
   } else {
-      if (menu) {
-          menu.style.transition = "right 0.2s ease-in-out";
-          menu.style.right = -menuWidth + "px";
-          let overlay: HTMLElement | null = document.querySelector('#overlay');
-          if (overlay) {
-              overlay.style.transition = "opacity 0.1s ease-in-out";
-              overlay.style.opacity = "1";
-              setTimeout(function() {
-                  if(overlay) overlay.style.opacity = "0"; // Trigger fade out.
-                  setTimeout(function() { 
-                      overlay?.parentNode?.removeChild(overlay);
-                  }, 100); // Remove after transition.
-              }, 0);
-          }
+    if (menu) {
+      menu.style.transition = "right 0.2s ease-in-out";
+      menu.style.right = -menuWidth + "px";
+      let overlay: HTMLElement | null = document.querySelector("#overlay");
+      if (overlay) {
+        overlay.style.transition = "opacity 0.1s ease-in-out";
+        overlay.style.opacity = "1";
+        setTimeout(function () {
+          if (overlay) overlay.style.opacity = "0"; // Trigger fade out.
+          setTimeout(function () {
+            overlay?.parentNode?.removeChild(overlay);
+          }, 100); // Remove after transition.
+        }, 0);
       }
+    }
   }
 }
