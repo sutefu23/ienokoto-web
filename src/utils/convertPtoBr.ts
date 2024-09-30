22235e6a118cfee33901be9b8704ac41390a8b6e
sutefu23 <sutefu23@gmail.com>
2024-09-30 14:33:51 +0900

    pタグ削除ロジック修正

--- src/utils/convertPtoBr.ts
+++ src/utils/convertPtoBr.ts
@@ -1,6 +1,6 @@
 export function convertPToBr(html: string): string {
   // 空の<p>タグ（&nbsp;を含む）を改行に置き換え
-  html = html.replace(/<p>\s*(&nbsp;)?\s*<\/p>/g, "<br>");
+  html = html.replace(/<p>/g, "").replace(/<\/p>/g, "<br>");
   html = html.replace(/<p>(.*?)<\/p>/g, "$1<br>");
   html = html.replace(/<br>$/, "");
 
